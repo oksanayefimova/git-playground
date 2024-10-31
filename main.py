401d708024a4f5c3ccf121a2fe4c710555d73c6e
oyefimova <oyefimova@kse.org.ua>
2024-10-31 10:12:03 +0200

    added losing message

--- main.py
+++ main.py
@@ -50,3 +50,8 @@ while not is_game_over():
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+    if ERRORS_TO_LOSE==errors:
+        print(':(')
+        print('You lose')
+        print(':(')
+        break
