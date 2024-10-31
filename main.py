a2c7906a56375701127ab03f429a3712ec7605da
oyefimova <oyefimova@kse.org.ua>
2024-10-31 10:06:06 +0200

    added improvements branch and validation

--- main.py
+++ main.py
@@ -11,12 +11,23 @@ def congratulate_user():
 def is_game_over():
     return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
 
+def input_validation(guess, words_duplicates):
+    while True:
+        if guess in words_duplicates:
+            print("You've already entered this word. Try a new one")
+            guess = input("Your next take: ")
+        else:
+            return False
+
+
 
 guessed = 0
 errors = 0
 
 WORDS_TO_WIN = 5
 ERRORS_TO_LOSE = 3
+
+words_duplicates = []
 
 words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
@@ -30,6 +41,8 @@ while not is_game_over():
     guess = input("Your next take: ")
     if guess in full_list:
         guessed += 1
+        input_validation(guess, words_duplicates)
+        words_duplicates.append(guess)
         if guessed == WORDS_TO_WIN:
             congratulate_user()
             exit()
